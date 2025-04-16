ca0d2f7d7dc93f2c52209bfa81824aa64ed038ab
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-04-16 11:00:28 -0300

    priemira tentativa desfazer_editor

--- desfazer_editor.c
+++ desfazer_editor.c
@@ -1,6 +1,21 @@
 #include "stdio.h"
 
-int main(){
-   char STR[100];
+void valida_opcao(char op[])
+{
+   if(op == "desfazer")
+   {
+
+   }
+   else if (op == "inserir")
+   {
+      
+   }
    
+}
+
+int main(){
+   char STR[100], opcão[10];
+   scanf("%s", opcão);
+   scanf("%100[^\n]");
+   return 0;
 }
