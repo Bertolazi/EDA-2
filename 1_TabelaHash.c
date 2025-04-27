3249f5b0848d3fd15a59325285fe4ec7593aabe2
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-04-27 18:29:27 -0300

    adicionando aula 1

---
+++ 1_TabelaHash.c
@@ -0,0 +1,4 @@
+/*
+    Nesse primeiro momento vamos introduzir a estrutura de dados denominada, estrutura de dispersão ou hash table. Essa estrutura é responsável por acelerar muitos algoritmos que envolvem consultas, inserções e remoções em uma tabela de dados.
+    
+*/
